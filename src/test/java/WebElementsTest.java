c390ad98161079ca09c14178ec188bda983a8201
Artur Przyjemski <spinezpl@gmail.com>
2020-08-18 19:58:23 +0200

    DropDownList added

--- src/test/java/WebElementsTest.java
+++ src/test/java/WebElementsTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -87,6 +88,13 @@ public class WebElementsTest {
         assertFalse(pizzaCheckbox.isSelected());
         assertFalse(spaghettiCheckbox.isSelected());
         assertFalse(hamburgerCheckbox.isSelected());
+    }
+
+    @Test
+    public void dropDownListTest(){
+        WebElement countryWebElement = driver.findElement(By.id("country"));
+
+        Select countryDropdown = new Select(countryWebElement);
 
 
     }
